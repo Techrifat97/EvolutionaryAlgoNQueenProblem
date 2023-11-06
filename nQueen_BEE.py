439928f6a86ca7dcbe7b57b8d3ec3a602eabf244
rifat ali shaon <rifatshaon.tech@gmail.com>
2023-11-06 05:30:38 +0100

    Comment added and minor input issues fixed

--- nQueen_BEE.py
+++ nQueen_BEE.py
@@ -1,15 +1,18 @@
 import random
 import time
+
+# Define parameter sets for the Bee Algorithm with different levels of intensity and search space.
 BEE_ALGORITHM_PARAMETER_SETS = {
+    # Parameter set 1 for smaller boards or quicker runs
     1: {"num_scouts": 50, "num_best_sites": 1, "num_bees_best_sites": 50, "num_other_sites": 1, "num_bees_other_sites": 50, "max_iterations": 500, "ngh": 50, "stlim": 50},
+    # Parameter set 2 for medium-sized boards or moderate runs
     2: {"num_scouts": 100, "num_best_sites": 2, "num_bees_best_sites": 100, "num_other_sites": 2, "num_bees_other_sites": 100, "max_iterations": 600, "ngh": 100, "stlim": 100},
+    # Parameter set 3 for larger boards or more intensive runs
     3: {"num_scouts": 150, "num_best_sites": 3, "num_bees_best_sites": 150, "num_other_sites": 3, "num_bees_other_sites": 150, "max_iterations": 700, "ngh": 150, "stlim": 150}
 }
-def initialize_board(n):
-    return [[str(i) for i in range(1, n + 1)] for _ in range(n)]
 
+# Function to calculate the cost (number of attacking pairs of queens)
 def cost(positions):
-    """Calculate the number of pairs of queens that are attacking each other"""
     n = len(positions)
     attack = 0
     for i in range(n):
@@ -18,37 +21,38 @@ def cost(positions):
                 attack += 1
     return attack
 
+# Function to generate a semi-random initial position based on a heuristic
 def heuristic_initial_positions(n):
-    """Generate a semi-random initial position based on a heuristic"""
     positions = list(range(n))
     random.shuffle(positions)
     return positions
 
+# Function to get user input for initial positions or generate random positions
 def get_user_input(n):
-    """Get user input for initial positions or generate random positions"""
-    choice = input("Do you want to input initial positions or use random initial positions? (input/random): ").strip().lower()
-    if choice == 'input':
+    choice = input("Do you want to input initial positions or use random initial positions? (yes/no): ").strip().lower()
+    if choice == 'yes':
         while True:
             try:
                 initial_positions = input(f"Enter the initial positions of the queens for a {n}x{n} board, separated by spaces (1-{n}): ")
                 positions = list(map(int, initial_positions.split()))
                 if len(positions) != n or not all(1 <= pos <= n for pos in positions):
                     raise ValueError
-                return positions
+                return positions  # Return the positions without checking for threats
             except ValueError:
-                print(f"Invalid input. Please enter {n} positions from 1 to {n}.")
-    elif choice == 'random':
+                print(f"Invalid input. Please enter {n} numbers from 1 to {n}, representing the row positions of the queens.")
+    elif choice == 'no':
         return heuristic_initial_positions(n)
     else:
         print("Invalid choice. Please enter 'input' or 'random'.")
         return get_user_input(n)
 
-def local_search(solution, n):
-    """Perform local search to improve a given solution"""
+# Function to perform local search to improve a given solution
+def local_search(solution, n, ngh):
     for col in range(n):
         min_conflicts = n
         best_row = solution[col]
-        for row in range(n):
+        # Explore the neighborhood of the current position within the range defined by ngh
+        for row in range(max(0, col - ngh), min(n, col + ngh + 1)):
             if row != solution[col]:
                 solution[col] = row
                 conflicts = cost(solution)
@@ -60,40 +64,25 @@ def local_search(solution, n):
             break  # Stop if we find a position with no conflicts
     return solution
 
+
+# Function to abandon sites that have not improved past a certain threshold
 def site_abandonment(solutions, costs, threshold):
-    """Abandon sites that have not improved past a certain threshold"""
-    return [(solution, cost) for solution, cost in zip(solutions, costs) if cost <= threshold]
+     return [(solution, cost) for solution, cost in zip(solutions, costs) if cost <= threshold]
 
-
-def neighbourhood_shrinking(solution, n, ngh):
-    """Reduce the neighborhood size to intensify the search"""
-    col = random.randint(0, n - 1)
-    best_row = solution[col]
-    min_conflicts = cost(solution)
-    for _ in range(ngh):
-        row_change = random.randint(-1, 1)
-        new_row = (solution[col] + row_change) % n
-        solution[col] = new_row
-        conflicts = cost(solution)
-        if conflicts < min_conflicts:
-            min_conflicts = conflicts
-            best_row = new_row
-    solution[col] = best_row
-    return solution
-
+# Function to perform a global search to escape local optima
 def global_search(num_scouts, n):
-    """Perform a global search to escape local optima"""
     return [heuristic_initial_positions(n) for _ in range(num_scouts)]
 
+# Main function implementing the Bees Algorithm for solving the N-Queens problem
 def bees_algorithm(n, num_scouts, num_best_sites, num_bees_best_sites, num_other_sites, num_bees_other_sites, max_iterations, ngh, stlim):
-    """The Bees Algorithm for solving the N-Queens problem"""
+    # Initialize scout solutions
     scout_solutions = [heuristic_initial_positions(n) for _ in range(num_scouts)]
     best_solution = None
     best_cost = float('inf')
-    some_interval = 10
-    some_threshold = 2  # This threshold could be a parameter or dynamically adjusted
+    some_interval = 10  # Interval for neighborhood shrinking
+    some_threshold = 2  # Threshold for site abandonment
     no_improvement_runs = 0
-    max_no_improvement_runs = stlim
+    max_no_improvement_runs = stlim  # Maximum runs without improvement before stopping
 
     for iteration in range(max_iterations):
         # Evaluate all scout solutions
@@ -104,18 +93,32 @@ def bees_algorithm(n, num_scouts, num_best_sites, num_bees_best_sites, num_other
         best_sites = sorted_solutions[:num_best_sites]
         for i, (solution, solution_cost) in enumerate(best_sites):
             for _ in range(num_bees_best_sites):
-                improved_solution = local_search(solution, n)
+                improved_solution = local_search(solution, n, ngh)
                 improved_cost = cost(improved_solution)
                 if improved_cost < solution_cost:
                     best_sites[i] = (improved_solution, improved_cost)
                     solution_cost = improved_cost
 
-        # Abandon sites that have not improved past the threshold
-        best_sites = site_abandonment([site for site, cost in best_sites], [cost for site, cost in best_sites], some_threshold)
+        # Select other sites and perform local search
+        other_sites = sorted_solutions[num_best_sites:num_best_sites+num_other_sites]
+        for i, (solution, solution_cost) in enumerate(other_sites):
+            for _ in range(num_bees_other_sites):
+                improved_solution = local_search(solution, n, ngh)
+                improved_cost = cost(improved_solution)
+                if improved_cost < solution_cost:
+                    other_sites[i] = (improved_solution, improved_cost)
+                    solution_cost = improved_cost
 
-        # Check for early termination
-        if best_sites:
-            current_best_solution, current_best_cost = min(best_sites, key=lambda x: x[1])
+        # Combine best and other sites after local search
+        combined_sites = best_sites + other_sites
+        combined_solutions = [site for site, _ in combined_sites]
+        combined_costs = [cost for _, cost in combined_sites]
+
+        # Abandon sites that have not improved past the threshold
+        combined_sites = site_abandonment(combined_solutions, combined_costs, some_threshold)
+        # Check for early termination if no improvement is observed
+        if combined_sites:
+            current_best_solution, current_best_cost = min(combined_sites, key=lambda x: x[1])
             if current_best_cost < best_cost:
                 best_solution, best_cost = current_best_solution, current_best_cost
                 no_improvement_runs = 0
@@ -124,27 +127,27 @@ def bees_algorithm(n, num_scouts, num_best_sites, num_bees_best_sites, num_other
                 if no_improvement_runs >= max_no_improvement_runs:
                     break
         else:
-            # If all sites are abandoned, then regenerate the scout solutions
+            # If all sites are abandoned, regenerate the scout solutions
             scout_solutions = global_search(num_scouts, n)
             continue
 
-        # Dynamic Neighborhood Shrinkage
+        # Perform neighborhood shrinking at certain intervals
         if iteration % some_interval == 0 and iteration > 0:
             for i in range(len(scout_solutions)):
-                scout_solutions[i] = neighbourhood_shrinking(scout_solutions[i], n, ngh)
+                scout_solutions[i] = local_search(scout_solutions[i], n, ngh)
 
-        # Replace the abandoned sites with new random scouts
-        while len(best_sites) < num_best_sites:
+        # Replace abandoned sites with new random scouts
+        while len(combined_sites) < num_best_sites + num_other_sites:
             new_scout = heuristic_initial_positions(n)
             new_cost = cost(new_scout)
-            best_sites.append((new_scout, new_cost))
+            combined_sites.append((new_scout, new_cost))
 
-        # Update the scout solutions with the best sites
-        scout_solutions = [solution for solution, cost in best_sites]
+        # Update scout solutions with the combined sites
+        scout_solutions = [solution for solution, cost in combined_sites]
 
     return best_solution
 
-
+# Function to check if a solution is valid (no queens are attacking each other)
 def is_solution_valid(position):
     n = len(position)
     if len(set(position)) != n:
@@ -155,7 +158,7 @@ def is_solution_valid(position):
                 return False
     return True
 
-# Example usage
+# Example usage of the algorithm
 if __name__ == "__main__":
     # Input board size with validation
     while True:
@@ -169,7 +172,7 @@ if __name__ == "__main__":
             print("Invalid input. Please enter an integer.")
 
     # Determine the number of algorithm runs
-    num_runs = 50  # Default value
+    num_runs = 50  # Default number of runs
     choice = input("Do you want to specify the number of times you want the algorithm to run?\n"
                    "The higher the number, the chance of getting multiple solutions increases (yes/no): ").strip().lower()
     if choice == 'yes':
@@ -177,6 +180,7 @@ if __name__ == "__main__":
         while num_runs <= 0:
             print("Number of runs must be a positive integer.")
             num_runs = int(input("Enter the number of runs: "))
+
     # Get initial positions from the user or generate random initial positions
     initial_positions = get_user_input(n)
     
